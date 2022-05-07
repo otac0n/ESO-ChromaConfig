1745bc28304fe553c19486fceb0b41f69125e92d
John Gietzen <john@gietzen.us>
2022-05-06 17:30:37 -0700

    Proof of concept to override alliance colors.

---
+++ ChromaConfig.lua
@@ -0,0 +1,13 @@
+﻿local ALLIANCE_COLORS = {
+  [ALLIANCE_ALDMERI_DOMINION] = ZO_ColorDef:New(1, 1, 0, 1),
+  [ALLIANCE_EBONHEART_PACT] = ZO_ColorDef:New(1, 0, 0, 1),
+  [ALLIANCE_DAGGERFALL_COVENANT] = ZO_ColorDef:New(0, 0, 1, 1),
+}
+
+local alliance = ZO_RZCHROMA_EFFECTS.activeAlliance
+local inBattleground = ZO_RZCHROMA_EFFECTS.inBattleground
+ZO_RZCHROMA_EFFECTS:SetAlliance(ALLIANCE_NONE)
+ZO_RZCHROMA_EFFECTS.allianceEffects = ZO_RZCHROMA_EFFECTS:CreateAllianceEffects(function (a)
+  return ALLIANCE_COLORS[a]
+end)
+ZO_RZCHROMA_EFFECTS:SetAlliance(alliance, inBattleground)
